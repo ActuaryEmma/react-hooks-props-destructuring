72fab7455388c088ac101e1b56c83ca1bc242734
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-08-01 09:35:57 +0300

    destructing props

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
+  
   const title = "Mad Max";
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr}  posterURL={posterURL}/>
+
     </div>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -9,7 +9,9 @@ function MovieCard({
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
+      
       <small>{genres.join(", ")}</small>
+
     </div>
   );
 }
